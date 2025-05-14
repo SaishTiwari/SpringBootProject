916e47133ae408c08ba6cd97db844e4396b5b0f2
SaishTiwari <tiwarisaish381@gmail.com>
2025-05-14 19:15:49 +0545

    Create Product

--- productlist/src/main/java/com/myfirstproject/productlist/controller/ProductController.java
+++ productlist/src/main/java/com/myfirstproject/productlist/controller/ProductController.java
@@ -1,14 +1,31 @@
 package com.myfirstproject.productlist.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.myfirstproject.productlist.dto.ProductDTO;
+import com.myfirstproject.productlist.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(name = "/api/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     //getAllProducts
+    
     //createProducts
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct (@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<> (productService.createProduct(productDTO), HttpStatus.CREATED);
+
+
+    }
+
     //updateProducts
     //deleteProducts
     //getProductsByID
--- productlist/src/main/java/com/myfirstproject/productlist/service/ProductService.java
+++ productlist/src/main/java/com/myfirstproject/productlist/service/ProductService.java
@@ -1,7 +1,48 @@
 package com.myfirstproject.productlist.service;
 
+import com.myfirstproject.productlist.controller.ProductController;
+import com.myfirstproject.productlist.dto.ProductDTO;
+import com.myfirstproject.productlist.entity.Category;
+import com.myfirstproject.productlist.entity.Product;
+import com.myfirstproject.productlist.mapper.ProductMapper;
+import com.myfirstproject.productlist.repository.CategoryRepository;
+import com.myfirstproject.productlist.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @Service
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    public ProductDTO createProduct (ProductDTO productDTO){
+
+        //Check if category exist or not
+        Category category = categoryRepository.findById(productDTO.getCategoryID())
+                . orElseThrow(()-> new RuntimeException("Category Not found"));
+
+        //DTO -> Entity
+        Product product = ProductMapper.toProductEntity(productDTO, category);
+
+        //Save Entity
+        Product savedProduct = productRepository.save(product);
+
+        //Entity -> DTO
+        return ProductMapper.toProductDTO(product);
+
+
+
+
+    }
+
+
 }
