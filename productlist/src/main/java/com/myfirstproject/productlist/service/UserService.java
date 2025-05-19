5f4028588ff3e1b04588dda14d2246489fa8e550
SaishTiwari <tiwarisaish381@gmail.com>
2025-05-19 22:07:43 +0545

    JWT token

--- productlist/src/main/java/com/myfirstproject/productlist/service/UserService.java
+++ productlist/src/main/java/com/myfirstproject/productlist/service/UserService.java
@@ -1,20 +1,29 @@
 package com.myfirstproject.productlist.service;
 
+import com.myfirstproject.productlist.dto.UserDTO;
 import com.myfirstproject.productlist.entity.User;
 import com.myfirstproject.productlist.repository.UserRepository;
 import com.myfirstproject.productlist.security.UserPrincipal;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -35,4 +44,6 @@ public class UserService implements UserDetailsService {
 
 
     }
+
+
 }
